8a5feee78bb875fe6dfee646f4838d5f959f5a48
NavyasriMerugu <129853460+NavyasriMerugu@users.noreply.github.com>
2023-04-12 11:11:24 +0530

    Add files via upload

---
+++ c programs/Pointers/pointertointeger.c
@@ -0,0 +1,32 @@
+// C program to understand difference between
+// pointer to an integer and pointer to an
+// array of integers.
+#include<stdio.h>
+
+int main()
+{
+	// Pointer to an integer
+	int *p;
+	
+	// Pointer to an array of 5 integers
+	int (*ptr)[5];
+	int arr[5];
+	
+	// Points to 0th element of the arr.
+	p = arr;
+	
+	// Points to the whole array arr.
+	ptr = &arr;
+	
+	printf("p = %p, ptr = %p\n", p, ptr);
+	
+	p++;
+	ptr++;
+	
+	printf("p = %p, ptr = %p\n", p, ptr);
+	
+	
+}
+ 
+output:p=0x7ffffcc00 ptr=ox7ffffcc00
+       p=0x7ffffcc04 ptr=0x7ffffcc14
