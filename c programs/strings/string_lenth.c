dbbe97ac5025a53cb4b0013eba57106c57adeef4
NavyasriMerugu <129853460+NavyasriMerugu@users.noreply.github.com>
2023-04-12 17:16:31 +0530

    Add files via upload

---
+++ c programs/strings/string_lenth.c
@@ -0,0 +1,23 @@
+// c program to print string lenth
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+ char str1[12] = "Hello";
+ char str2[12] = "World";
+ char str3[12];
+ int len ;
+ 
+ strcpy(str3, str1);
+ printf("strcpy( str3, str1) : %s\n", str3 );
+ 
+ strcat( str1, str2);
+ printf("strcat( str1, str2): %s\n", str1 );
+ len = strlen(str1);
+ printf("strlen(str1) : %d\n", len );
+ return 0;
+}
+//output:
+//strcpy( str3, str1) : Hello
+//strcat( str1, str2): HelloWorld
+//strlen(str1) : 10
