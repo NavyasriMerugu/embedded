726b1f639e2f55f61f910a24e7afcca4735ec580
NavyasriMerugu <129853460+NavyasriMerugu@users.noreply.github.com>
2023-04-12 12:48:53 +0530

    Add files via upload

---
+++ c programs/Arrays/armstrong_or_not.c.c
@@ -0,0 +1,21 @@
+//c program to find the no is armstrong or not
+ #include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");      
+}   
+//out put :enter the number=153
+         //armstrong number
